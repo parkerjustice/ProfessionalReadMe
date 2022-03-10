1c95d5ee4be47c17ace469efa193ec008f7b1ae2
parkerjustice <you@example.com>
2022-03-09 21:55:07 -0500

    added js

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require("./generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What project is this?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("Please enter a name");
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
